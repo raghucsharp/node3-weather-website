4fd72c4bd0dbc5552bf6ab86a51510a1446db529
Raghunath Madduri <raghunathmadduri@MacBook-Pro.local>
2020-09-13 14:30:38 +0530

    Setup app for Heroku

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ const forecast = require('./utils/forecast');
 console.log(__dirname);
 console.log(path.join(__dirname, '../public'));
 const app = express();
+const port = process.env.PORT || 3000;
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public');
@@ -77,6 +78,6 @@ app.get('*', (req, res) => {
         name: 'Andrew Mead', errorMessage: 'Page Not Found'
     });
 });
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log('Server is up on port: 3000');
 });
